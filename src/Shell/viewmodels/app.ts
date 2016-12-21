2ededafd9fa2f75603dff149f141fee83a72ecd3
David Kossoglyad <david@logoui.co.il>
2016-12-21 18:09:08 +0200

    Added "Document Approval" route

--- src/Shell/viewmodels/app.ts
+++ src/Shell/viewmodels/app.ts
@@ -1,3 +1,4 @@
+import { DocumentsApproval } from './../../DocumentsApproval/viewmodels/documentsApproval';
 import { Scanner } from './../../Scanner/viewmodels/scanner';
 import {Aurelia} from 'aurelia-framework';
 import {Router, RouterConfiguration} from 'aurelia-router';
@@ -10,7 +11,7 @@ export class App {
     config.title = 'Aurelia';
     config.map([
       { route: ['', 'scanner'], name: 'Scanner',      moduleId: '../../Scanner/viewmodels/scanner.ts',      nav: true, 		title: 'Scan Documents' }
-    //, { route: 'docsForAprovement', name: 'docsForAprovement',      moduleId: './docsForAprovement',      nav: true, title: 'Documents Aprovement' }
+    , { route: 'documentsApproval', name: 'DocumentsApproval',      moduleId: '../../DocumentsApproval/viewmodels/documentsApproval.ts',      nav: true, title: 'Documents Approval' }
     ]);
 
     this.router = router;
